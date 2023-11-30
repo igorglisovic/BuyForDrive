23af4ae328a053e47a48b674e3fbed83194df996
Gliso <igorglisovic2@gmail.com>
2023-11-30 19:40:57 +0100

    Added loading spinner to car[id] page

--- app/car/[id]/page.jsx
+++ app/car/[id]/page.jsx
@@ -14,9 +14,10 @@ import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import moment from 'moment/moment'
 import { useRouter } from 'next/navigation'
+import LoadingSpinner from '@app/components/ui/LoadingSpinner'
 
 const CarPage = ({ params }) => {
-  const { data } = useFetch(`/api/car/${params.id}`)
+  const { data, loading } = useFetch(`/api/car/${params.id}`)
   const car = data && data[0]
 
   const router = useRouter()
@@ -75,72 +76,78 @@ const CarPage = ({ params }) => {
           />
           <span className="text-sm mt-2">Posted: {relativeTime}</span>
         </div>
-        <div className="grid grid-cols-car gap-8">
-          <div className="flex flex-col gap-8 mt-10 col-span-2 md-plus:col-span-1">
-            <CarSlider car={car} />
-            <CarDetails car={car} />
-            <CarInformation car={car} />
-            <CarAdditionalInfo car={car} />
-            <CarDescription car={car} />
-          </div>
-          <aside className="w-full md:mt-10 md:block row-start-4 col-span-2 md-form:row-start-1 md-form:col-start-2">
-            <div className="flex flex-col items-center gap-3 bg-white p-6 rounded-[30px] shadow-xl">
-              {car?.creator.image && (
-                <Image
-                  className="w-[80px] h-[80px] rounded-full"
-                  width={80}
-                  height={80}
-                  alt="avatar"
-                  src={car.creator.image}
+        {!loading ? (
+          <div className="grid grid-cols-car gap-8">
+            <div className="flex flex-col gap-8 mt-10 col-span-2 md-plus:col-span-1">
+              <CarSlider car={car} />
+              <CarDetails car={car} />
+              <CarInformation car={car} />
+              <CarAdditionalInfo car={car} />
+              <CarDescription car={car} />
+            </div>
+            <aside className="w-full md:mt-10 md:block row-start-4 col-span-2 md-form:row-start-1 md-form:col-start-2">
+              <div className="flex flex-col items-center gap-3 bg-white p-6 rounded-[30px] shadow-xl">
+                {car?.creator.image && (
+                  <Image
+                    className="w-[80px] h-[80px] rounded-full"
+                    width={80}
+                    height={80}
+                    alt="avatar"
+                    src={car.creator.image}
+                  />
+                )}
+                <span className="text-lg capitalize">
+                  {car?.creator.username}
+                </span>
+                {session?.user.id === car?.creator._id && (
+                  <>
+                    <div className="flex flex-col gap-0 items-center">
+                      <Link
+                        className="underline hover:text-gray-500"
+                        href={`/edit-car?id=${car?._id}`}
+                      >
+                        Edit Car
+                      </Link>
+                      <button
+                        className="underline text-red-500 hover:text-red-400"
+                        onClick={handleDelete}
+                      >
+                        Remove Car
+                      </button>
+                    </div>
+                    <button className="bg-btn-2 hover:bg-gray-200 py-2 px-8 rounded-full font-semibold">
+                      <Link href="/profile">Check dealer</Link>
+                    </button>
+                  </>
+                )}
+                {session?.user.id !== car?.creator._id && (
+                  <button className="bg-btn-2 hover:bg-gray-200 py-2 px-8 rounded-full font-semibold">
+                    <Link
+                      href={`/profile/${car?.creator._id}?name=${car?.creator.username}`}
+                    >
+                      Check profile
+                    </Link>
+                  </button>
+                )}
+              </div>
+            </aside>
+            <div className="flex flex-col col-span-2">
+              {otherCars && (
+                <CarsSlider
+                  cars={otherCars}
+                  title="Other cars from this seller"
                 />
               )}
-              <span className="text-lg capitalize">
-                {car?.creator.username}
-              </span>
-              {session?.user.id === car?.creator._id && (
-                <>
-                  <div className="flex flex-col gap-0 items-center">
-                    <Link
-                      className="underline hover:text-gray-500"
-                      href={`/edit-car?id=${car?._id}`}
-                    >
-                      Edit Car
-                    </Link>
-                    <button
-                      className="underline text-red-500 hover:text-red-400"
-                      onClick={handleDelete}
-                    >
-                      Remove Car
-                    </button>
-                  </div>
-                  <button className="bg-btn-2 hover:bg-gray-200 py-2 px-8 rounded-full font-semibold">
-                    <Link href="/profile">Check dealer</Link>
-                  </button>
-                </>
-              )}
-              {session?.user.id !== car?.creator._id && (
-                <button className="bg-btn-2 hover:bg-gray-200 py-2 px-8 rounded-full font-semibold">
-                  <Link
-                    href={`/profile/${car?.creator._id}?name=${car?.creator.username}`}
-                  >
-                    Check profile
-                  </Link>
-                </button>
-              )}
             </div>
-          </aside>
-          <div className="flex flex-col col-span-2">
-            {otherCars && (
-              <CarsSlider
-                cars={otherCars}
-                title="Other cars from this seller"
-              />
-            )}
+            <div className="flex flex-col col-span-2">
+              <CarsSlider cars={similarCars} title="Similar cars" />
+            </div>
           </div>
-          <div className="flex flex-col col-span-2">
-            <CarsSlider cars={similarCars} title="Similar cars" />
+        ) : (
+          <div className="h-[700px] mt-10">
+            <LoadingSpinner />
           </div>
-        </div>
+        )}
       </Container>
     </div>
   )
