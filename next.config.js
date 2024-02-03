b639ccb838e56cee318d0124d4171e8143d528ac
Gliso <igorglisovic2@gmail.com>
2024-02-03 22:24:57 +0100

    Debugging

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  cacheHandler: require.resolve('./cache-handler.js'),
-  cacheMaxMemorySize: 0, // disable default in-memory caching
   experimental: {
     serverComponentsExternalPackages: ['mongoose'],
   },
