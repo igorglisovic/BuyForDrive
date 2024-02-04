5ed1eb40d87661b0d87b3cbce585e316597c0b2f
Gliso <igorglisovic2@gmail.com>
2024-02-04 01:03:03 +0100

    Debugging deploy error

--- next.config.js
+++ next.config.js
@@ -23,13 +23,13 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    }
-    return config
-  },
+  // webpack(config) {
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     topLevelAwait: true,
+  //   }
+  //   return config
+  // },
 }
 
 module.exports = nextConfig
